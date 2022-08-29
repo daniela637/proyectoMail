a5ef38202b0e8071ddc30ea901d3893ecd151372
daniela637 <danii_9997@hotmail.com>
2022-08-28 21:00:25 -0300

    documentado el codigo

--- src/java/modeloDAO/UsuarioDAO.java
+++ src/java/modeloDAO/UsuarioDAO.java
@@ -1,4 +1,3 @@
-
 package modeloDAO;
 
 import Interfaces.ValidarUsuario;
@@ -8,41 +7,46 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import modelo.Usuario;
 
+public class UsuarioDAO implements ValidarUsuario {
 
-
-public class UsuarioDAO implements ValidarUsuario{
-
- Connection con;
- Conection conexion = new Conection();
-   PreparedStatement ps;
+    Connection con;
+    Conection conexion = new Conection();
+    PreparedStatement ps;
     ResultSet rs;
-    int resultado =0;
+    int resultado = 0;
 
+    
+    /**
+     * recibe como parametro un objeto de tipo usuario, donde va a comparar si lo que recibio
+     * coincide con lo que esta en la BDD, y retorna 1 si coincide o 0 si no lo hace
+     * @param usu
+     * @return 
+     */
     @Override
     public int validarUsuario(Usuario usu) {
-       int resultado =0;
-    
-       String sql = "Select mail from usuario where mail=? ";
-       
-       try{
-           con = conexion.getConection();
-           ps= con.prepareStatement(sql);
-           ps.setString(1, usu.getMail());
-           rs = ps.executeQuery();
-           
-           while(rs.next()){
-               resultado = resultado +1;
-               usu.setMail(rs.getString("mail"));
-              
-           }
-           if(resultado ==1){
-               return 1;
-           }else{
-               return 0;
-           }
-       }catch(Exception e){
-           return 0;
-       }
+        int resultado = 0;
+
+        String sql = "Select mail from usuario where mail=? ";
+
+        try {
+            con = conexion.getConection();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, usu.getMail());
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                resultado = resultado + 1;
+                usu.setMail(rs.getString("mail"));
+
+            }
+            if (resultado == 1) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } catch (Exception e) {
+            return 0;
+        }
     }
-    
+
 }
